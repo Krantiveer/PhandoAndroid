6dd400ee3f7cc6226ed28567e55a48dfe9d1a514
Triloki Nath Yadav <trilokinathyadav@qainfotech.com>
2020-08-03 13:11:29 +0530

    ads and notification inprogress

---
+++ app/src/test/java/com/perseverance/phando/NewTest.java
@@ -0,0 +1,22 @@
+package com.perseverance.phando;
+
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NewTest {
+
+    @Test
+    public String getOtpFromSMS(String message) {
+        String OTP_REGEX = "(|^)\\d{4}";
+        Pattern pattern  = Pattern.compile(OTP_REGEX);
+        Matcher matcher  = pattern.matcher(message);
+        String otp = "Not found";
+        while (matcher.find()) {
+            otp = matcher.group();
+            System.out.println(otp);
+        }
+        return otp;
+    }
+}

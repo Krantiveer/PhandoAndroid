fe40fc5df106955993ecdcfca38dff3b84c9bbe8
sh7verma <39428657+sh7verma@users.noreply.github.com>
2022-03-15 18:18:01 +0530

    ADN

--- app/src/adn/java/com/perseverance/phando/Constants.java
+++ app/src/adn/java/com/perseverance/phando/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 
     public static final boolean BUILD_PROD = BuildConfig.BUILD_TYPE.equalsIgnoreCase("PROD");
 
-    public static final String BASE_URL_PROD = "http://ott.adnbroadband.com/";
+    public static final String BASE_URL_PROD = "https://ott.adnbroadband.com/";
     public static final String BASE_URL_STAGE = "https://phunflixqa.phando.com/";
 
     public static final String GA_TRACKER_ID = "UA-152515869-1";
@@ -17,6 +17,5 @@ public class Constants {
     public static final String URL_ABOUT_US = new FeatureConfigClass().getBaseUrl() + "page?type=aboutus";
     public static final String URL_TC = new FeatureConfigClass().getBaseUrl() + "page?type=termconditions";
     public static final String URL_PRIVACY_POLICY = new FeatureConfigClass().getBaseUrl() + "page?type=privacy";
-
 
 }
